ccf0614872f72befd5ff6ec1266cb346b080810a
Nathan Shelly <nathanshelly2018@u.northwestern.edu>
2016-05-28 14:30:05 -0500

    Removed driver functions in bayes

--- bayes.py
+++ bayes.py
@@ -172,33 +172,3 @@ def bulk_test(dict_of_catalogs, divisor = 1, path = 'books/'):
     print 'Final Accuracies: '
     for genre in temp_results.keys():
         print genre + ': ', temp_results[genre]
-
-##################################### Main
-
-def drive_cross_validate():
-    genres = ['Mystery', 'Vampires']
-    folds = 4
-    books_path = 'books/'
-    smoothing_factor = 1
-
-    results, accuracies = cross_validate(genres, folds, books_path, smoothing_factor)
-
-    genre_string = ''
-    for genre in genres:
-        genre_string += genre + '_'
-    f = open('results/' + genre_string + folds + '_fold_' + smoothing_factor + '_smooth', 'w')
-    f.write('Averages: ' + str(results) + '\n')
-    f.write('Per-fold accuracies: ' + str(accuracies) + '\n')
-    print 'Accuracies: ', accuracies
-    print 'Results: ', results
-
-def test_smooth_values():
-    path = 'catalogs/'
-    # smoothed_catalog_path = 'catalogs_smoothed'
-    catalogs = {'Teen': load(path + 'Teen.p'), 'Horror': load(path + 'Horror.p')}
-    words = word_list(catalogs)
-    for s_f in [1, .5, .25]:
-        catalogs = smooth(catalogs, words, s_f)
-        for key in catalogs.keys():
-            catalogs[key] = generate_percentile_catalog(catalogs[key])
-    bulk_test(catalogs, divisor = 10)
